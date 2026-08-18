c8435f9dc5a2d1fa1e0a9c828adf3609b5ca6e07
Rota PMMG <leiajacintoalves@gmail.com>
2026-08-17 21:39:02 -0300

    Corrige placeholder inválido de Processo Penal

--- lessons-processo-penal.js
+++ lessons-processo-penal.js
@@ -1,10 +1,7 @@
 "use strict";
 
-window.PMMG_LESSONS =
-window.PMMG_LESSONS || {};
+window.PMMG_LESSONS = window.PMMG_LESSONS || {};
 
-// ...
-
-Object.assign(window.PMMG_LESSONS,{
-   ...
-});
+// Conteúdo de Processo Penal ainda não foi publicado neste arquivo.
+// Mantemos o arquivo válido para não interromper o carregamento dos demais módulos.
+Object.assign(window.PMMG_LESSONS, {});
